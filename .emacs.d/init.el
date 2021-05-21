9d9cea79b4bca11dba2fca511350fd577030bc5b
Han-Miru Kim <hanmiru.kim@gmail.com>
2021-05-21 18:07:33 +0200

    emacs init

---
+++ .emacs.d/init.el
@@ -0,0 +1,7 @@
+;; Emacs Configuration file
+
+;; Disable startup screen
+(setq inhibit-startup-message t)
+
+;; Font settings
+;;(set-face-attribute 'default nil :font "DejaVu Sans Regular" : height 40)
